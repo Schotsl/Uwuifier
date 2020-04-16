a3050d43008a9afc74ef30850ee476fef6db6f1d
Schotsl <sjorsvanholst@gmail.com>
2020-04-16 19:41:34 +0200

    Added test output, random sentences/words and refactored

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,13 +1,47 @@
 import { uwufyWord, uwufySentence, uwuFace } from '../index';
 
-test('Uwufy a word', () => {
-  expect(typeof uwufyWord('Boat')).toBe('string');
-  expect(uwufyWord('Trees').length).toBeGreaterThanOrEqual('Trees'.length);
+const senteces = [
+  'I had to get a tetanus vaccine when i stabbed my foot by accident',
+  'When you say tonight, at what time approximately do you mean',
+  'I need someone to go through the coop thing with me',
+  'I\'m really struggling to put this story toghether, you we\'re chilling with a dude who has decided that woman sucks',
+  'No there has been this lockdown in the Netherlands so check out this site: https://www.who.int/health-topics',
+  'Wow you\'re old as fuck, you\'re basically my grandpa you know!'
+]
+
+const words = [
+  'Stabbed',
+  'Tonight',
+  'Through',
+  'Struggling',
+  'Netherlands',
+  'Grandpa'
+]
+
+function getElement(array: string[]): string {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+console.log(`Please take a look if these senteces like uwuified:`)
+
+test(`Uwufy a word`, () => {
+  const normalWord = getElement(words);
+  const uwuifiedWord = uwufyWord(normalWord);
+  
+  console.log(`\nUwuified word 📘: ${uwuifiedWord}`);
+
+  expect(typeof uwuifiedWord).toBe('string');
+  expect(uwuifiedWord.length).toBeGreaterThanOrEqual(normalWord.length);
 });
 
 test('Uwufy a sentence', () => {
-  expect(typeof uwufySentence('Tom handed in a blank test paper.')).toBe('string');
-  expect(uwufySentence('I made some mistakes in life').length).toBeGreaterThanOrEqual('Trees'.length);
+  const normalSentence = getElement(senteces);
+  const uwufiedSentence = uwufySentence(normalSentence);
+
+  console.log(`\nUwuified sentence 📔: ${uwufiedSentence}`)
+
+  expect(typeof uwufiedSentence).toBe('string');
+  expect(uwufiedSentence.length).toBeGreaterThanOrEqual(normalSentence.length);
 });
 
 test('Get an uwu face', () => {
