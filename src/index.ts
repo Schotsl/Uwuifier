3368025e13a9cea446740a455a586f2dc064bc39
Schotsl <sjorsvanholst@gmail.com>
2020-04-18 23:39:01 +0200

    Added getter and setters

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 'use strict';
 
-const faces = [`(・\`ω´・)`, `;;w;;`, `owo`, `UwU`, `>w<`, `^w^`, `ÚwÚ`, `:3`, `x3`];
-const actions = [
+export let faces: string[] = [`(・\`ω´・)`, `;;w;;`, `owo`, `UwU`, `>w<`, `^w^`, `ÚwÚ`, `:3`, `x3`];
+export let actions: string[] = [
   `*blushes*`,
   `*whispers to self*`,
   `*sweats*`,
@@ -11,7 +11,7 @@ const actions = [
   `*boops your nose*`,
   `*starts twerking*`
 ];
-const exclimations = [`?!!`, `?!?1`, `!!11`, `?!?!`, `!?`];
+export let exclimations: string[] = [`?!!`, `?!?1`, `!!11`, `?!?!`, `!?`];
 
 function getElement(array: string[]): string {
   return array[Math.floor(Math.random() * array.length)];
@@ -21,6 +21,50 @@ function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// All get functions
+export function getExclimations(): string[] {
+  return exclimations;
+}
+export function getActions(): string[] {
+  return actions;
+}
+export function getFaces(): string[] {
+  return faces;
+}
+
+// All set functions
+export function setExclimations(newExclimations: string[]): void {
+  exclimations = newExclimations;
+}
+export function setActions(newActions: string[]): void {
+  actions = newActions;
+}
+export function setFaces(newFaces: string[]): void {
+  faces = newFaces;
+}
+
+// All add functions
+export function addExclimation(newExclimation: string): void {
+  exclimations.push(newExclimation);
+}
+export function addAction(newAction: string): void {
+  actions.push(newAction);
+}
+export function addFace(newFace: string): void {
+  faces.push(newFace);
+}
+
+// All remove functions
+export function removeExclimation(removedExclimation: string): void {
+  exclimations = exclimations.filter((exclimation) => exclimation !== removedExclimation);
+}
+export function removeAction(removedAction: string): void {
+  actions = actions.filter((action) => action !== removedAction);
+}
+export function removeFace(removedFace: string): void {
+  faces = faces.filter((face) => face !== removedFace);
 }
 
 export function uwuifyWord(word: string): string {
@@ -35,7 +79,7 @@ export function uwuifyWord(word: string): string {
 
 export function uwuifySentence(sentence: string): string {
   // Replace normal question marks and exclamations with more 'expressive' characters
-  sentence = sentence.replace(new RegExp('[?!]+$'), getElement(exclimations));
+  if (exclimations.length) sentence = sentence.replace(new RegExp('[?!]+$'), getElement(exclimations));
 
   // Spwit the sentence into wowds
   const words = sentence.split(` `);
@@ -53,17 +97,17 @@ export function uwuifySentence(sentence: string): string {
     const random = Math.random();
 
     // 5% chance of getting a random face
-    if (random <= 0.05) {
+    if (random <= 0.05 && faces.length) {
       uwuified += ` ${getElement(faces)}`;
-    // 5% chance of a getting a random action
-    } else if (random <= 0.10) {
+      // 5% chance of a getting a random action
+    } else if (random <= 0.1 && actions.length) {
       uwuified += ` ${getElement(actions)}`;
-    // 10% chance of stutter if the word hasn't been uwuified before for readability
-    } else if (random <= 0.20 && !isUwuified && !isUrl) {
+      // 10% chance of stutter if the word hasn't been uwuified before for readability
+    } else if (random <= 0.2 && !isUwuified && !isUrl) {
       const letter = normalWord[0];
       const stutter = getRandomInt(0, 2);
 
-      for (let i = 0; i < stutter; i ++) {
+      for (let i = 0; i < stutter; i++) {
         uwuifiedWord = `${letter}-${uwuifiedWord}`;
       }
     }
