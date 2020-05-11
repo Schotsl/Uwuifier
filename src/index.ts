5f8ef1a9a7d1cf021d11b0a835876a3758b33787
Schotsl <sjorsvanholst@gmail.com>
2020-05-11 22:12:40 +0200

    Implemented modifier values

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@
 
 import { getElement, getRandomInt, getCapitalPercentage } from './utils';
 
+interface spaceModifier {
+  facePercentage: number;
+  actionPercentage: number;
+  stutterPercentage: number;
+}
+
 export class Uwuifier {
   public faces: string[] = [`(・\`ω´・)`, `;;w;;`, `owo`, `UwU`, `>w<`, `^w^`, `ÚwÚ`, `:3`, `x3`];
   public actions: string[] = [
@@ -16,26 +22,31 @@ export class Uwuifier {
   ];
   public exclimations: string[] = [`?!!`, `?!?1`, `!!11`, `?!?!`, `!?`];
 
+  private wordsModifier: number = 1;
+  private spacesModifier: spaceModifier = { facePercentage: 0.05, actionPercentage: 0.05, stutterPercentage: 0.1 };
+  private exclimationsModifier: number = 1;
+
   public uwuifyWords(sentence: string): string {
     let uwuifiedSentence = ``;
 
     // Split the string into words
     const words = sentence.split(` `);
+    const random = Math.random();
     const pattern = new RegExp(/(?:https?|ftp):\/\/[\n\S]+/g);
 
-    words.forEach((word) => {
+    words.forEach((wordValue) => {
       // If word is a URL don't uwuifiy it
-      if (pattern.test(word)) {
-        word = word.replace(/(?:r|l)/g, `w`);
-        word = word.replace(/(?:R|L)/g, `W`);
-        word = word.replace(/n([aeiou])/g, `ny$1`);
-        word = word.replace(/N([aeiou])/g, `Ny$1`);
-        word = word.replace(/N([AEIOU])/g, `Ny$1`);
-        word = word.replace(/ove/g, `uv`);
+      if (pattern.test(wordValue)) {
+        if (random <= this.wordsModifier) wordValue = wordValue.replace(/(?:r|l)/g, `w`);
+        if (random <= this.wordsModifier) wordValue = wordValue.replace(/(?:R|L)/g, `W`);
+        if (random <= this.wordsModifier) wordValue = wordValue.replace(/n([aeiou])/g, `ny$1`);
+        if (random <= this.wordsModifier) wordValue = wordValue.replace(/N([aeiou])/g, `Ny$1`);
+        if (random <= this.wordsModifier) wordValue = wordValue.replace(/N([AEIOU])/g, `Ny$1`);
+        if (random <= this.wordsModifier) wordValue = wordValue.replace(/ove/g, `uv`);
       }
 
       // Reconstruct the string with uwuified words
-      uwuifiedSentence += ` ${word}`;
+      uwuifiedSentence += ` ${wordValue}`;
     });
 
     return uwuifiedSentence;
@@ -54,15 +65,19 @@ export class Uwuifier {
       let insertedExpression = false;
       let removeCapital = false;
 
-      if (random <= 0.05 && this.faces.length) {
+      const faceThreshold = this.spacesModifier.facePercentage;
+      const actionThreshold = this.spacesModifier.actionPercentage + faceThreshold;
+      const stutterThreshold = this.spacesModifier.stutterPercentage + actionThreshold;
+
+      if (random <= faceThreshold && this.faces.length) {
         // Add random face before the word
         uwuifiedSentence += ` ${getElement(this.faces)}`;
         insertedExpression = true;
-      } else if (random <= 0.1 && this.actions.length) {
+      } else if (random <= actionThreshold && this.actions.length) {
         // Add random action before the word
         uwuifiedSentence += ` ${getElement(this.actions)}`;
         insertedExpression = true;
-      } else if (random <= 0.2) {
+      } else if (random <= stutterThreshold) {
         // Add stutter with a length between 0 and 2 length
         const letter = wordValue[0];
         const stutter = getRandomInt(0, 2);
@@ -104,8 +119,28 @@ export class Uwuifier {
   }
 
   public uwuifyExclimations(sentence: string): string {
-    if (this.exclimations.length) sentence = sentence.replace(new RegExp('[?!]+$'), getElement(this.exclimations));
-    return sentence;
+    let uwuifiedSentence = ``;
+
+    // Split the string into words
+    const words = sentence.split(` `);
+    const pattern = new RegExp('[?!]+$');
+
+    words.forEach((wordValue, wordIndex) => {
+      const random = Math.random();
+
+      // If there are exclimations replace them
+      if (pattern.test(wordValue)) {
+        if (random <= this.exclimationsModifier) {
+          wordValue = wordValue.replace(pattern, ``);
+          wordValue += getElement(this.exclimations);
+        }
+      }
+
+      // Reconstruct the string
+      uwuifiedSentence += ` ${wordValue}`;
+    });
+
+    return uwuifiedSentence;
   }
 
   public uwuifySentence(sentence: string): string {
