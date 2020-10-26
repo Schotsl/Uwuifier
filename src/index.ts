47d3df7843c3ba22d9b504ddd536a83b5ae2f6b2
BadBoyHaloCat <nutdriver716@gmail.com>
2020-10-25 19:49:26 -0700

    Use `??` instead of a ternary typeof.
    TSC will compile it with Babel to something that will work pre-node-14

--- src/index.ts
+++ src/index.ts
@@ -45,16 +45,13 @@ export class Uwuifier {
       exclamations: 1
     }
   ) {
-    this._spacesModifier =
-      typeof spaces !== `undefined`
-        ? spaces
-        : {
+    this._spacesModifier = spaces ?? {
             faces: 0.05,
             actions: 0.075,
             stutters: 0.1
           };
-    this._wordsModifier = typeof words !== `undefined` ? words : 1;
-    this._exclamationsModifier = typeof exclamations !== `undefined` ? exclamations : 1;
+    this._wordsModifier = words ?? 1;
+    this._exclamationsModifier = exclamations ?? 1;
   }
 
   public uwuifyWords(sentence: string): string {
