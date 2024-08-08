3c9deff956d36bb2392aae7ed591ad7fcb6ab4bf
Sjors van Holst <sjorsvanholst@gmail.com>
2024-08-08 19:57:47 +0200

    ⬆️ Upgraded more dependencies

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.82.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.224.0/testing/asserts.ts";
 
 import Seed from "./seed.ts";
 import Uwuifier from "./index.ts";
